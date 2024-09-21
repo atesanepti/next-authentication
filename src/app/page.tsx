e264b6f34e36565abae953adc3d3d495c632a0fc
san-bin-hoque <epti060@gmail.com>
2024-09-21 16:06:33 +0600

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="bg-black w-[100%] h-screen flex items-center flex-col justify-center">
       <h1 className="text-white text-3xl font-bold ">Hello Dear! </h1>
       <span className="text-white text-2xl font-semibold">What about you?</span>
-      <Link href="/login">Go to the Login Page</Link>
+      <Link className="bg-white text-black rounded-md px-4 py-2 cursor-pointer" href="/login">Go to the Login Page</Link>
     </div>
   );
 }
