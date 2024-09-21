a6761cdb06c68f04ed202a7bfa27fb8991a4a97e
san-bin-hoque <epti060@gmail.com>
2024-09-21 16:01:25 +0600

    build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react' 
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button';
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 const page = () => {
   return (
     <div className="bg-black w-full h-screen flex items-center justify-center dark">
@@ -8,12 +8,10 @@ const page = () => {
         <Input placeholder="Email" type="email" name="email" />
         <br />
         <Input placeholder="Password" type="password" name="password" />
-        <Button variant="ghost">
-            Login
-        </Button>
+        <Button>Login</Button>
       </div>
     </div>
   );
-}
+};
 
-export default page
+export default page;
