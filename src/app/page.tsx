224f53b560df0d63e894e3b73bc0d0cbb50a810a
san-bin-hoque <epti060@gmail.com>
2024-09-21 16:03:56 +0600

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link"
 export default function Home() {
   return (
     <div className="bg-black w-[100%] h-screen flex items-center flex-col justify-center">
       <h1 className="text-white text-3xl font-bold ">Hello Dear! </h1>
       <span className="text-white text-2xl font-semibold">What about you?</span>
+      <Link href="/login">Go to the Login Page</Link>
     </div>
   );
 }
